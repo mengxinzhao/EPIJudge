bbfe09ffeddab0b1d836831a5bc921b4ca6c0844
mengxin_zhao <springvesper@gmail.com>
2018-02-18 01:18:20 -0800

    pass enumerate balanced parenthese. Not efficient had to use set to keep out  duplicates

--- epi_judge_cpp/enumerate_balanced_parentheses.cc
+++ epi_judge_cpp/enumerate_balanced_parentheses.cc
@@ -1,12 +1,41 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
+#include <set>
 using std::string;
 using std::vector;
 
+void GenerateBalancedParenthesesHelper(int start, int num_pairs, string &current, std::set<string> & result) {
+    if (start >=num_pairs)
+        return ;
+    for (int k = start; start < num_pairs; start ++)
+        for (int i=start+1; i<num_pairs; i++) {
+            if (current[start*2+1]!=current[2*i]) {
+                std::swap(current[start*2+1],current[2*i]);
+                //std::cout<<current<<std::endl;
+                result.insert(current);
+                GenerateBalancedParenthesesHelper(start+1, num_pairs,current,result);
+                std::swap(current[start*2+1],current[2*i]);
+            }
+    }
+}
+
 vector<string> GenerateBalancedParentheses(int num_pairs) {
   // Implement this placeholder.
-  return {};
+  
+    vector<string> result{};
+    if (num_pairs == 0)
+        return {("")};
+    string first{};
+    std::set<string> result_set;
+    for (int i=0;i< num_pairs;i++)
+        first.insert(0,"()");
+    result_set.insert(first);
+    GenerateBalancedParenthesesHelper(0,num_pairs,first, result_set);
+    for (auto &e:result_set)
+        result.push_back(e);
+    return result;
 }
 
 #include "test_framework/test_utils_generic_main.h"
@@ -18,3 +47,5 @@ int main(int argc, char* argv[]) {
       &GenerateBalancedParentheses, &UnorderedComparator<vector<string>>);
   return 0;
 }
+
+
