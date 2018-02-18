f980876dc1149edbc3d58835f92c9fd9311cec5f
mengxin_zhao <springvesper@gmail.com>
2018-02-18 14:19:22 -0800

    solved number of ways to climbe staircase. Passed judge

--- epi_judge_cpp/number_of_traversals_staircase.cc
+++ epi_judge_cpp/number_of_traversals_staircase.cc
@@ -1,8 +1,19 @@
-
-
+#include <iostream>
+#include <vector>
+// count the number of moves to climb staircases
 int NumberOfWaysToTop(int top, int maximum_step) {
-  // Implement this placeholder.
-  return 0;
+    // Implement this placeholder.
+    // NumberOfWaysToTop(N) = NumberOfWaysToTop(N-1)
+    //                  + NumberOfWaysToTop(N-2) + ... + NumberOfWaysToTop(N-max_step)
+    std::vector<int> sumOfStepsForStep(top+1,0);
+    sumOfStepsForStep[0] = 1;
+    sumOfStepsForStep[1] = 1;
+    for (int i =2 ; i <=top; i++) {
+        for ( int j = std::max(i-maximum_step,0);j<i && j>=0;j++)
+            sumOfStepsForStep[i] += sumOfStepsForStep[j];
+    }
+
+  return sumOfStepsForStep[top];
 }
 
 #include "test_framework/test_utils_generic_main.h"
