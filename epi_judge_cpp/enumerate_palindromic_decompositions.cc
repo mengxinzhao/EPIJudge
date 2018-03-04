84a5bcffdb87fd0a8c5881f497c4621c4a4fda0a
mengxin_zhao <springvesper@gmail.com>
2018-03-03 23:17:01 -0800

    solved enumerate palindromic decomposition in a string

--- epi_judge_cpp/enumerate_palindromic_decompositions.cc
+++ epi_judge_cpp/enumerate_palindromic_decompositions.cc
@@ -2,13 +2,54 @@
 #include <iterator>
 #include <string>
 #include <vector>
-
+#include <cstddef>
+#include <iostream>
 using std::string;
 using std::vector;
 
+bool isPalindrome(const string &input, size_t start, size_t end)
+{
+    for (size_t i = start; i <= (start+end)/2; i++)
+        if (input[i] != input[end - i])
+            return false;
+    return true;
+}
+
+// aabac
+// a | abac|
+//          --> a | a | bac
+//                         --> a | a | b | ac
+//                                      --> a | a | b | a | c
+//          --> a | aba | c
+// aa | bac
+//         --> aa | b| ac
+//                       --> aa | b | a | c
+
+// recursively decomposite string as palindrom_prefix | the rest to the end of the string
+void PalindromeDecompositionsHelper(const string &input,vector<string> current, vector<vector<string>> &result)
+{
+    if (input.empty()) {
+        result.emplace_back(current);
+        return ;
+    }
+
+    for (size_t prefix_length=1; prefix_length <=input.length();prefix_length++){
+        if (isPalindrome(input, 0,prefix_length-1)) {
+            string prefix(string(input.begin(), input.begin() +prefix_length));
+            current.push_back(prefix);
+            PalindromeDecompositionsHelper(string(input.begin() + prefix_length, input.end()),current,result);
+            current.pop_back();
+        }
+    }
+}
+
 vector<vector<string>> PalindromeDecompositions(const string& input) {
   // Implement this placeholder.
-  return {};
+    vector<vector<string>> result({});
+    vector<string> current{};
+    
+    PalindromeDecompositionsHelper(input,current,result);
+    return result;
 }
 
 bool Comp(vector<vector<string>> expected, vector<vector<string>> result) {
