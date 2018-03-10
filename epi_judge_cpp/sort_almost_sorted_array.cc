68422c28b2e0071de76e443a31af8404a99915dd
mengxin_zhao <springvesper@gmail.com>
2018-03-10 13:41:49 -0800

    solve sort_almost_sorted_array on 11.3. judge data passe

--- epi_judge_cpp/sort_almost_sorted_array.cc
+++ epi_judge_cpp/sort_almost_sorted_array.cc
@@ -1,16 +1,37 @@
 #include <vector>
+#include <queue>
+#include <iterator>
 
 using std::vector;
-
+// Build a min heap with k elements
+// keep poping the top and inserting new element will iterating to the end of the sequence
+// O(log(k))  insertion complexity
+// O(Nlog(k)) total complexity
 vector<int> SortApproximatelySortedData(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end, int k) {
-  // Implement this placeholder.
-  return {};
+    
+    std::priority_queue<int, vector<int>,std::greater<int>> min_heap;
+    vector<int> result;
+    // build min heap first
+    for (auto it = sequence_begin; it != sequence_begin + k; it++) {
+        min_heap.emplace(*it);
+    }
+    auto it = sequence_begin + k;
+    while(it != sequence_end) {
+        result.push_back(min_heap.top());
+        min_heap.pop();
+        min_heap.emplace(*it);
+        it++;
+    }
+    while(!min_heap.empty()) {
+        result.push_back(min_heap.top());
+        min_heap.pop();
+    }
+    return result;
 }
 
-vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,
-                                               int k) {
+vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,int k) {
   return SortApproximatelySortedData(cbegin(sequence), cend(sequence), k);
 }
 
