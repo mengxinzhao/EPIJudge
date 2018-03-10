e4ce9e93386de575729fd6abca3536803a357235
mengxin_zhao <springvesper@gmail.com>
2018-03-10 13:19:23 -0800

    solve count inversion in 16.13

--- epi_judge_cpp/count_inversions.cc
+++ epi_judge_cpp/count_inversions.cc
@@ -1,13 +1,46 @@
 #include <vector>
 #include <iostream>
+#include <cstddef>
 using std::vector;
 
-int countInversions(vector<int> &A, int left, int right) {
-	if (left >= right)
-		return 0;
+// merge sort and count inversion when merging
+// return index array instead of  value array
+// O(NLog(N))
+vector<int> countInversionsHelper(vector<int> &A, int &inversion_count, int left, int right) {
+	if (left == right)
+		return vector<int>(1,left);
 
-	int mid = (left + right)/2;   // assume there is no overflow
-	return countInversions(A,left,mid) + countInversions(A,mid+1,right) + ((A[mid] > A[mid+1])?1:0);
+	size_t mid = (left + right)/2;
+
+    vector<int> partition_left = countInversionsHelper(A, inversion_count,left, mid);
+    vector<int> partition_right =countInversionsHelper(A,inversion_count,mid+1, right);
+    // merge and count inversion
+    vector<int> merged(partition_left.size()+partition_right.size(),0);
+    size_t i = 0, j=0,k=0;
+
+    while(i < partition_left.size() && j< partition_right.size()) {
+        if (A[partition_left[i]] <= A[partition_right[j]] ) {
+            merged[k++] = partition_left[i++];
+        }else {
+             if (partition_left[i] < partition_right[j])
+                 // from i to mid in the left part,  all biggher than A[partition_right[j]]. all inversion
+                 inversion_count += partition_left.size() - i ;
+             merged[k++] = partition_right[j++];
+
+        }
+    }
+    
+    if(i < partition_left.size() && j== partition_right.size()) {
+        while (i < partition_left.size()) {
+            merged[k++] = partition_left[i++];
+        }
+    }
+    if(j< partition_right.size() && i == partition_left.size() ){
+        while (j < partition_right.size()) {
+            merged[k++] = partition_right[j++];
+        }
+    }
+    return merged;
 }
 
 
@@ -15,9 +48,12 @@ int countInversions(vector<int> &A, int left, int right) {
 int CountInversions(vector<int> A) {
   // Implement this placeholder.
 
-	//total inversion number = count(0,mid) + count(mid+1,end) + count(mid, mid+1);
-	int result = countInversions(A,0,A.size()-1);
-  return result;
+    int num_inversion = 0;
+	vector<int> merged = countInversionsHelper(A, num_inversion, 0,A.size()-1);
+//    for (const auto &e : merged)
+//        std::cout<<A[e]<<" ";
+//    std::cout<<std::endl;
+    return num_inversion;
 }
 
 #include "test_framework/test_utils_generic_main.h"
