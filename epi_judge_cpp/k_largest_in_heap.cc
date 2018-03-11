92a335983ef31dfb92196b0f069410c1fa304b15
mengxin_zhao <springvesper@gmail.com>
2018-03-10 20:52:33 -0800

    solve k_largest_in_heap in 11.6. judge data passed

--- epi_judge_cpp/k_largest_in_heap.cc
+++ epi_judge_cpp/k_largest_in_heap.cc
@@ -1,10 +1,38 @@
 #include <vector>
+#include <queue>
+#include <utility>
+#include <iostream>
 
 using std::vector;
 
+//buid a max heap and iterate through A
+//pop top =  A[i]
+//push A[2*i+1], A[2*i+2] children into the heap
+//repeat till k pops.
+//complexity O(kLog(k))
+template<typename T>
+class Compare {
+public:
+    bool operator() (T one, T two) {
+        return one.first < two.first;
+    }
+};
 vector<int> KLargestInBinaryHeap(const vector<int>& A, int k) {
-  // Implement this placeholder.
-  return {};
+    std::priority_queue<std::pair<int,int>,vector<std::pair<int,int>>, Compare<std::pair<int,int>>>max_heap;
+    vector<int> result;
+    max_heap.emplace(std::make_pair(A[0],0));
+    
+    while(result.size()<k) {
+        auto top = max_heap.top();
+        max_heap.pop();
+        result.push_back(top.first);
+        if ( 2*top.second+1 < A.size())
+            max_heap.emplace(std::make_pair(A[2*top.second+1], 2*top.second+1));
+        if ( 2*top.second+2 < A.size())
+            max_heap.emplace(std::make_pair(A[2*top.second+2], 2*top.second+2));
+    }
+    
+    return result;
 }
 
 #include "test_framework/test_utils_generic_main.h"
