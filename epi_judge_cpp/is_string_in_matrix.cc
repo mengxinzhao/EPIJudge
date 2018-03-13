c2bed8a235177346b6d5d6efc5ac887290fe6188
mengxin_zhao <springvesper@gmail.com>
2018-03-13 16:17:25 -0700

    simplify the codes a little bit. Still inefficient

--- epi_judge_cpp/is_string_in_matrix.cc
+++ epi_judge_cpp/is_string_in_matrix.cc
@@ -4,7 +4,6 @@
 #include <set>
 #include <utility>
 #include <map>
-#include <iterator>
 
 using std::vector;
 
@@ -26,32 +25,31 @@ public:
 // O(4^N) N: table size
 // not efficient at all. worst running time when pattern is long duplication of the grid
 // [[55,55,55]]  ==> patter to be searched [55,55,55,55,55...]
-bool matchHelper(int match_offset, std::pair<int,int>curr_loc, MatchStatus& curr,
+bool matchHelper(const vector<vector<int>>& grid,
+                 const vector<int>& pattern,
+                 int match_len,
+                 int i, int j,
                  std::map<std::pair<int,int>, MatchStatus>&tbl){
 
-    if (curr.matches.find(match_offset) == curr.matches.end()) {
-        return false;
-    }
-    else {
-        if (match_offset-1 ==0)
+    if (grid[i][j] == pattern[match_len-1]) {
+        if ((match_len+1)>pattern.size())
             return true;
-        //see if they match match_offset -1
-        int i = curr_loc.first;
-        int j = curr_loc.second;
-
         bool new_match = false;
-        if (tbl.find(std::make_pair(i+1,j))!= tbl.end())
-            new_match |= matchHelper(match_offset-1, std::pair<int,int>({i+1,j}),tbl.find(std::make_pair(i+1,j))->second, tbl );
-        if (tbl.find(std::make_pair(i-1,j))!= tbl.end())
-            new_match |= matchHelper(match_offset-1, std::pair<int,int>({i-1,j}),tbl.find(std::make_pair(i-1,j))->second, tbl );
-        if (tbl.find(std::make_pair(i,j+1))!= tbl.end())
-            new_match |= matchHelper(match_offset-1, std::pair<int,int>({i,j+1}),tbl.find(std::make_pair(i,j+1))->second, tbl );
-        if (tbl.find(std::make_pair(i,j-1))!= tbl.end())
-            new_match |= matchHelper(match_offset-1, std::pair<int,int>({i,j-1}),tbl.find(std::make_pair(i,j-1))->second, tbl );
+        if (i+1 < grid.size())
+            new_match |= matchHelper(grid,pattern, match_len+1, i+1,j,tbl );
+        if (i-1 >=0 )
+            new_match |= matchHelper(grid,pattern, match_len+1, i-1,j,tbl );
+        if (j+1 < grid[i].size())
+            new_match |= matchHelper(grid,pattern, match_len+1, i,j+1,tbl );
+        if (j-1 >=0)
+            new_match |= matchHelper(grid,pattern, match_len+1, i,j-1,tbl );
         return new_match;
-    }
+        
+    }else
+        return false;
     
 }
+
 
 bool IsPatternContainedInGrid(const vector<vector<int>>& grid,
                               const vector<int>& pattern) {
@@ -59,22 +57,13 @@ bool IsPatternContainedInGrid(const vector<vector<int>>& grid,
     std::map<std::pair<int,int>, MatchStatus>tbl;
     
     // table constructions takes O(grid_size* pattern_size)
+    size_t len =0;
     for (size_t i=0;i < grid.size();i++)
         for (size_t j =0; j< grid[i].size();j++) {
-            MatchStatus s = MatchStatus();
-            for (size_t len =0; len < pattern.size();len++) {
-                if (grid[i][j] == pattern[len]) {
-                    s.update(len+1);
-                }
-            }
-            if (!s.matches.empty())
-                tbl.insert(std::make_pair(std::make_pair(i,j),s));
-        }
-    
-    for (auto &iter:tbl) {
-        if (matchHelper(pattern.size(), iter.first, iter.second,tbl))
-            return true;
+            if (matchHelper(grid,pattern,1,i,j,tbl))
+                return true;
     }
+    
 
     return false;
 }
