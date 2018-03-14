00890cf761c7e459a49ae4a45b1fc2a872c26c95
mengxin_zhao <springvesper@gmail.com>
2018-03-14 16:46:17 -0700

    improve running time further by reducing steps to consider

--- epi_judge_cpp/advance_by_offsets.cc
+++ epi_judge_cpp/advance_by_offsets.cc
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <cstddef>
+#include <algorithm>
 
 using std::vector;
 
@@ -9,18 +10,23 @@ using std::vector;
 bool CanReachEnd(const vector<int>& max_advance_steps) {
     vector<int> tbl(max_advance_steps.size(),0);
     tbl[0]=1;
-    
+
+    size_t max_step = *std::max_element(max_advance_steps.begin(), max_advance_steps.end());
+    max_step = std::min(max_advance_steps.size(),max_step);
     // build up the cache
     // reachable(i,j) = reachable(i,j-step) && max_advance_steps[j-step] >= step
-    for (int i =1 ; i < max_advance_steps.size();i++)
-        for (int step = 1 ; step <=i;step++) {
-            tbl[i] += (tbl[i-step] && max_advance_steps[i-step] >=step);
-            //std::cout<<"0 to "<<i <<" through "<< i-step << " reachable: "<<tbl[i] << std::endl;
+    for (size_t i =1 ; i < max_advance_steps.size();i++) {
+        int max_distance = std::min(i,max_step);
+        for (int step = 1 ; step <=max_distance;step++) {
+            tbl[i] += (tbl[i-step] > 0 && max_advance_steps[i-step] >= step);
+            //std::cout<<"step: "<< step << " 0 to "<<i <<" through "<< i-step << " reachable: "<<tbl[i] << std::endl;
         }
-            
+    }
     return tbl[max_advance_steps.size()-1];
 
 }
+
+
 
 #include "test_framework/test_utils_generic_main.h"
 
