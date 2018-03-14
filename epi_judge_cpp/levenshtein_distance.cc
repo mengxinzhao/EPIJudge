82709677e4e5204e12a231cb2da19a434575507a
mengxin_zhao <springvesper@gmail.com>
2018-03-13 23:17:42 -0700

    ugly solution for levenshtein_distance. very slow

--- epi_judge_cpp/levenshtein_distance.cc
+++ epi_judge_cpp/levenshtein_distance.cc
@@ -1,12 +1,30 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::string;
 using std::vector;
 
+// editting distance from A to B
+int editDistance(const string &A, int i, const string &B, int j) {
+    if( i == 0 )
+        return j; // all insertion
+    
+    if (j == 0)
+        return i; // all deletion
+    
+    if (A[i-1] == B[j-1])
+        return editDistance(A, i-1,B,j-1);
+    
+    //consider all possible solutions
+    return 1+ std::min(std::min(editDistance(A,i-1,B,j),  //A[i] deleted
+                       editDistance(A,i, B, j-1)),      // B[i] deleted
+                       editDistance(A,i-1, B, j-1));  //change
+}
 int LevenshteinDistance(const string& A, const string& B) {
-  // Implement this placeholder.
-  return 0;
+  
+
+    return  editDistance(A,A.length(), B, B.length());
 }
 
 #include "test_framework/test_utils_generic_main.h"
