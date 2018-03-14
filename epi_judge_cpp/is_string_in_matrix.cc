43cd4e7b60c44e1f12897fcc365acb6cad67e235
mengxin_zhao <springvesper@gmail.com>
2018-03-13 18:21:48 -0700

    optimized the performance by using set data structure that is caching the unsuccessful matches

--- epi_judge_cpp/is_string_in_matrix.cc
+++ epi_judge_cpp/is_string_in_matrix.cc
@@ -4,60 +4,63 @@
 #include <set>
 #include <utility>
 #include <map>
+#include <tuple>
+#include <unordered_set>
 
 using std::vector;
+using std::tuple;
+using std::get;
+using std::unordered_set;
+using std::hash;
+using std::set;
 
 struct MatchStatus {
-public:
-    std::set <int> matches;
-    
-    MatchStatus():matches({}) {}
-
-    MatchStatus &update(int new_offset) {
-        if (matches.find(new_offset) == matches.end())
-            matches.insert(new_offset);
-        return *this;
-    }
+    int x, y;   // start location
+    int match_distance ;// max distance in the pattern it can be matched starting from x,y
 };
+
+
+//struct HashTuple {
+//    size_t operator()(const tuple<int, int, int>& t) const {
+//        return hash<int>()(get<0>(t) ^ get<1>(t) * 1021 ^ get<2>(t) * 1048573);
+//    }
+//};
 
 // When the current match_offset is matched recursively
 // 4 directions in the table if match_offset is matched
 // O(4^N) N: table size
 // not efficient at all. worst running time when pattern is long duplication of the grid
 // [[55,55,55]]  ==> patter to be searched [55,55,55,55,55...]
+// use a cached table to store all the unmatched location
 bool matchHelper(const vector<vector<int>>& grid,
                  const vector<int>& pattern,
                  int match_len,
                  int i, int j,
-                 std::map<std::pair<int,int>, MatchStatus>&tbl){
+                 set<tuple<int,int,int>> &tbl){
 
-    if (grid[i][j] == pattern[match_len-1]) {
-        if ((match_len+1)>pattern.size())
-            return true;
-        bool new_match = false;
-        if (i+1 < grid.size())
-            new_match |= matchHelper(grid,pattern, match_len+1, i+1,j,tbl );
-        if (i-1 >=0 )
-            new_match |= matchHelper(grid,pattern, match_len+1, i-1,j,tbl );
-        if (j+1 < grid[i].size())
-            new_match |= matchHelper(grid,pattern, match_len+1, i,j+1,tbl );
-        if (j-1 >=0)
-            new_match |= matchHelper(grid,pattern, match_len+1, i,j-1,tbl );
-        return new_match;
-        
-    }else
+    
+    if (pattern.size () < match_len)
+        return true;
+    if (i < 0 || i >=grid.size() || j < 0 || j >=grid[i].size() ||
+        tbl.find({i, j, match_len}) != tbl.cend() ||
+        grid[i][j] != pattern[match_len-1]) {
         return false;
+    }
+    if (matchHelper(grid,pattern, match_len+1, i+1,j,tbl )||matchHelper(grid,pattern, match_len+1, i-1,j,tbl)
+            ||matchHelper(grid,pattern, match_len+1, i,j+1,tbl )|| matchHelper(grid,pattern, match_len+1, i,j-1,tbl ))
+        return true;
+        
+    tbl.emplace(i,j,match_len);
+    return false;
     
 }
 
 
 bool IsPatternContainedInGrid(const vector<vector<int>>& grid,
                               const vector<int>& pattern) {
+    // use just set is faster instead of unordered_set with customer hash
+    set<tuple<int,int,int>>tbl;
     
-    std::map<std::pair<int,int>, MatchStatus>tbl;
-    
-    // table constructions takes O(grid_size* pattern_size)
-    size_t len =0;
     for (size_t i=0;i < grid.size();i++)
         for (size_t j =0; j< grid[i].size();j++) {
             if (matchHelper(grid,pattern,1,i,j,tbl))
