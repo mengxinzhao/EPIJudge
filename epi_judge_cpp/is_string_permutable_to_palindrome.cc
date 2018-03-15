a25a6d5dd764175687e8ef8a182f180c29644625
mengxin_zhao <springvesper@gmail.com>
2018-03-14 23:46:46 -0700

    solve is_string_permutable_to_palindrome. judge data passed. no hash used

--- epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -1,9 +1,34 @@
 #include <string>
-
+#include <unordered_map>
+#include <vector>
 using std::string;
+using std::vector;
+using std::unordered_map;
 
+//even length string can't have char appearing more than 1 odd times
+//odd length string must have all char apeparning even times
 bool CanFormPalindrome(const string& s) {
-  // Implement this placeholder.
+    unordered_map<char, vector<int>> tbl;
+    for (int i=0;i<s.length();i++) {
+        tbl[s[i]].push_back(i);
+    }
+    if (s.length()%2) {
+        //odd length at most 1 char appearing odd time
+        int odd_times = 0;
+        for (auto &iter: tbl){
+            if (iter.second.size()%2) {
+                odd_times+=1;
+                if (odd_times>=2)
+                    return false;
+            }
+        }
+    }else {
+        // even length. each char needs to be appearing even times
+        for (auto &iter: tbl){
+            if (iter.second.size()%2)
+                return false;
+        }
+    }
   return true;
 }
 
