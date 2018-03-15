d06104eadc25b93bd6153e85a6e7c7ef4d3d7020
mengxin_zhao <springvesper@gmail.com>
2018-03-15 11:20:43 -0700

    solve element_appearing_once by using hash table

--- epi_judge_cpp/element_appearing_once.cc
+++ epi_judge_cpp/element_appearing_once.cc
@@ -1,10 +1,23 @@
 #include <vector>
+#include <unordered_map>
 
 using std::vector;
+using std::unordered_map;
 
+// O(N+m) complexity
+// N size of A, m size of unique element in A
 int FindElementAppearsOnce(const vector<int>& A) {
-  // Implement this placeholder.
-  return 0;
+    unordered_map<int, vector<int>> tbl;
+    
+    for (int i=0; i < A.size();i++) {
+        tbl[A[i]].push_back(i);
+    }
+    
+    for (const  auto &iter: tbl ) {
+        if (iter.second.size()==1)
+            return iter.first;
+    }
+    return 0;
 }
 
 #include "test_framework/test_utils_generic_main.h"
