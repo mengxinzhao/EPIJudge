29a099f0f95880ef99a67c4ec17d3b18451a8e44
mengxin_zhao <springvesper@gmail.com>
2018-03-15 00:08:09 -0700

    reduce space used in unordered_map.

--- epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -6,30 +6,23 @@ using std::vector;
 using std::unordered_map;
 
 //even length string can't have char appearing more than 1 odd times
-//odd length string must have all char apeparning even times
+//odd length string must have all char appearing even times
 bool CanFormPalindrome(const string& s) {
-    unordered_map<char, vector<int>> tbl;
+    unordered_map<char, int> tbl;
     for (int i=0;i<s.length();i++) {
-        tbl[s[i]].push_back(i);
+        tbl[s[i]]++;
+    }
+    // at the most one char appearing odd times
+    int count = 0;
+    for (auto &iter: tbl) {
+        if (iter.second %2 )
+            count++;
     }
     if (s.length()%2) {
-        //odd length at most 1 char appearing odd time
-        int odd_times = 0;
-        for (auto &iter: tbl){
-            if (iter.second.size()%2) {
-                odd_times+=1;
-                if (odd_times>=2)
-                    return false;
-            }
-        }
+        return count <=1;
     }else {
-        // even length. each char needs to be appearing even times
-        for (auto &iter: tbl){
-            if (iter.second.size()%2)
-                return false;
-        }
+        return count ==0;
     }
-  return true;
 }
 
 #include "test_framework/test_utils_generic_main.h"
