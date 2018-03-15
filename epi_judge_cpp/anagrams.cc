3632f722501afa40781f1688528b1147dda41ee8
mengxin_zhao <springvesper@gmail.com>
2018-03-14 23:08:58 -0700

    optimize it by using unordered_map to keep all index in the directionary whoose string is anagram of another

--- epi_judge_cpp/anagrams.cc
+++ epi_judge_cpp/anagrams.cc
@@ -3,13 +3,13 @@
 #include <functional>
 #include <cstddef>
 #include <algorithm>
-#include <map>
+#include <unordered_map>
 #include <utility>
 #include <iostream>
 using std::string;
 using std::vector;
 using std::pair;
-using std::map;
+using std::unordered_map;
 
 struct Compare {
     bool operator() (pair<size_t, int> &one ,pair<size_t, int> &two) {
@@ -29,35 +29,23 @@ size_t HushString (string item ){
 
 
 vector<vector<string>> FindAnagrams( const vector<string>& dictionary) {
-  // Implement this placeholder.
-    vector<pair<size_t, int>> tbl;   // map hush back to index
+    unordered_map<size_t, vector<int>> tbl;   // map hush back to index
     vector<vector<string>> result;
     for (size_t len = 0; len < dictionary.size();len++) {
         size_t val = HushString(dictionary[len]);
-        tbl.push_back(std::make_pair(val,len));
+        tbl[val].push_back(len);
+    }
+    
+    for (auto &iter: tbl) {
+        if (iter.second.size()>=2){
+            vector<string> current;
+            for (auto &index: iter.second) {
+                current.push_back(dictionary[index]);
+            }
+            result.push_back(current);
+        }
     }
 
-    std::sort(tbl.begin(), tbl.end(), compare_hush);
-    vector<string> current;
-    // grab the string that its hush value appear at twice in the array
-    for (size_t len = 0; len < tbl.size()-1;len++) {
-        current.push_back(dictionary[tbl[len].second]);
-        if (tbl[len].first != tbl[len+1].first) {
-            result.push_back(current);
-            current.clear();
-        }
-    }
-    if (tbl[tbl.size()-1].first == tbl[tbl.size()-2].first ) {
-        current.push_back(dictionary[tbl[tbl.size()-1].second]);
-        result.push_back(current);
-    }
-    // delete
-    for (int i=0; i< result.size();i++) {
-        if (result[i].size() <2) {
-            result.erase(result.begin()+i);
-            i--;
-        }
-    }
     return result;
     
 }
