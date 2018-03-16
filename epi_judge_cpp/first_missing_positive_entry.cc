fa1de053ff9ddc93593e21d6fc934f0635f6c003
mengxin_zhao <springvesper@gmail.com>
2018-03-16 01:44:49 -0700

    revisit first_missing_positive_entry.cc. fixed crash

--- epi_judge_cpp/first_missing_positive_entry.cc
+++ epi_judge_cpp/first_missing_positive_entry.cc
@@ -6,28 +6,32 @@ using std::vector;
 
 // A is passed by value argument, since we change it.
 int FindFirstMissingPositive(vector<int> A) {
-	// Implement this placeholder.
-	vector<int> &v = A;
 	// 1st pass separate negative 0 and positive
 	int pos = 0;
-	for (int i =0; i < v.size()-1; i++) {
-		if (v[i] <=0) {
-			std::swap(v[i],v[pos++]);
+	for (int i =0; i < A.size(); i++) {
+		if (A[i] <=0) {
+			std::swap(A[i],A[pos++]);
 		}
 	}
-	std::cout<<"pos: "<<pos<<std::endl;
+	//std::cout<<"pos: "<<pos<<std::endl;
 
 	// positive numbers are after pos
-	// adjust array such as A[i] = i+1-pos
-	for (int i = pos;i < v.size();i++) {
-		while (v[i] -1 +pos  < v.size() && v[i]!=v[v[i] -1 +pos]) {
-			std::swap(v[i],v[v[i]-1+pos]);
+	// adjust array such that A[i] = A[A[i]+1-pos]
+	for (int i = pos;i < A.size();i++) {
+        while ((A[i] -1 +pos ) < A.size() &&A[i] -1 +pos>=0 &&  A[i]!=A[A[i] -1 +pos]) {
+			std::swap(A[i],A[A[i]-1+pos]);
 		}
 	}
 	// the first element A[i]!=i+1-pos shows that it misses i+1-pos
-	for (int i=pos; i< v.size();i++)
-		if (v[i]!= i+1-pos)
-			return i+1-pos;
+    if (pos < A.size()) {
+        for (int i=pos; i< A.size();i++)
+            if (A[i] != i+1-pos)
+                return i+1-pos;
+        
+        return A.size()+1-pos;
+    } else
+        return 1;
+
 }
 
 #include "test_framework/test_utils_generic_main.h"
