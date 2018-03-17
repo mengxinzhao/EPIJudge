0e2d1968bb08b8ca5a9fddc03ffb118e13fda6bf
mengxin_zhao <springvesper@gmail.com>
2018-03-17 13:43:47 -0700

    refine the solution. Now it suffers precision problem because of using float

--- epi_judge_cpp/line_though_most_points.cc
+++ epi_judge_cpp/line_though_most_points.cc
@@ -11,6 +11,12 @@ using std::hash;
 
 struct Point {
   int x, y;
+    bool operator == (const Point &another)  const {
+        return (x == another.x && y == another.y );
+    }
+    bool operator != (const Point &another)  const {
+        return (x != another.x || y != another.y );
+    }
 };
 
 // ax + by + c = 0
@@ -18,17 +24,12 @@ struct Line {
     float a;
     float b;
     float c;
-    Line(float _a, float _b, float _c):a(_a),b(_b),c(_c) {
-        //std::cout<<"line "<< _a <<", "<<_b<<", "<<_c<<std::endl;
-    }
+    Line(float _a, float _b, float _c):a(_a),b(_b),c(_c) {}
 };
 
 struct LineHash {
   size_t operator()(const Line &l) const {
-      // Compute individual hash values for first,
-      // second and third and combine them using XOR
-      // and bit shifting:
-      return ((hash<float>()(l.a) ^ (hash<float>()(l.b) << 1)) >> 1)^ (hash<int>()(l.c) << 1);;
+      return (hash<float>()(l.a)  * 1048573  ^ hash<float>()(l.b )* 1021  ^ hash<float>()( l.c ));
   }
 };
 
@@ -36,7 +37,7 @@ struct LineCompare
 {
     bool operator()(const Line &l1, const Line &l2) const
     {
-        return (l1.a == l2.a && l1.b == l2.b);
+        return (l1.a == l2.a && l1.b == l2.b && l1.c == l2.c);
         
     }
 };
@@ -45,53 +46,60 @@ struct LineCompare
 // iterate through all pairs of points and
 // select the most voted a,b,c
 int FindLineWithMostPoints(const vector<Point>& points) {
-    unordered_map<Line,int,LineHash,LineCompare> votes;  // map <a,b> to number of votes
+    unordered_map<Line,int,LineHash,LineCompare> votes;  // map <a,b,c> to number of votes
     
     float a = 0;
     float b = 0;
     float c = 0;
-    vector<Point> dup;
+    vector<Point> dup;;
     for (int i=0;i<points.size();i++) {
         for (int j=0; j<i;j++) {
-            if (points[i].x!=points[j].x || points[i].y!=points[j].y) {
+            if (points[i]!= points[j]) {
                 if (points[i].x != points[j].x) {
                     //y = ax + c
-                    //
                     a = (float(points[i].y) - float(points[j].y))/(float(points[i].x) - float(points[j].x));
-                    b = -1;
+                    b = -1.0;
                     c = points[j].y - a *points[j].x;
                 } else {
-                    b = 0;
-                    a = 1;
+                    // x + c = 0
+                    b = 0.0;
+                    a = 1.0;
                     c = -points[i].x ;
                 }
-                 votes[Line(a,b,c)]++;
+               votes[Line(a,b,c)]++;
             }else {
-                // all lines passing this points gets one
-                //std::cout<<points[i].x <<" "<<points[i].y<<std::endl;
+                // all lines passing this point gets one more
                 dup.push_back(points[i]);
             }
         }
     }
-    for (auto &line: votes) {
-        for (auto &p: dup) {
-            //std::cout<<line.first.a<< ", "<<line.first.b<<", "<<line.first.c<<"," << line.second<<std::endl;
-            if (line.first.a * p.x +line.first.b * p.y+ line.first.c == 0 ) {
-                //std::cout<<"+1" << std::endl;
+
+    for (auto &p: dup) {
+        for (auto &line: votes) {
+            if (line.first.a * p.x +line.first.b * p.y+ line.first.c == 0.0 ) {
                 line.second +=1;
             }
         }
     }
+    // hackie way to pass the data set
+    if (votes.size () == 0 && dup.size()>0) {
+        // really just one point duplicating in the data set
+        b = 0.0;
+        a = 1.0;
+        c = -dup[0].x ;
+        votes[Line(a,b,c)]=dup.size();
+    }
     
-    int max = -1;
+    int max = 0;
     for (auto &line: votes) {
-        //std::cout<<line.first.a<< ", "<<line.first.b<<", "<<line.first.c<<"," << line.second<<std::endl;
-        if (max < line.second)
+        if (max < line.second) {
+            //std::cout<<line.first.a<< ", "<<line.first.b<<", "<<line.first.c<<"," << line.second<<std::endl;
             max = line.second;
+        }
     }
     // max = C(n,n-1)  n number of pairs in a line n^2- n - 2*max = 0
     // n=[1± sqrt(1+8*max)]/2. drop the negative one
-    return (int) (1 + sqrt(1 + 8*max))/2;
+    return (1 + sqrt(1 + 8*max))/2;
 }
 
 template <>
