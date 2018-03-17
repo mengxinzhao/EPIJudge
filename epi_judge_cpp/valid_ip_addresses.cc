b85c7b52ea036aa5e75d7fca6f0ca24ee843cc8c
mengxin_zhao <springvesper@gmail.com>
2018-03-16 18:22:48 -0700

    initial solution for valid_ip_addresses failed at test cases 3 where the judge data doesn't allow multiple 0s

--- epi_judge_cpp/valid_ip_addresses.cc
+++ epi_judge_cpp/valid_ip_addresses.cc
@@ -1,13 +1,42 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <cstddef>
+#include <iostream>
 
 using std::string;
 using std::vector;
+using std::stoi;
+
+void GetValidIpAddressHelper (const string &s, vector<string> & result,
+                              int start, int num_parts, string current ) {
+
+    if (start == s.length() && num_parts == 4) {
+        result.push_back(current);
+    }else if (start < s.length() && num_parts >=4 )
+        return;
+    else {
+        // from start of the string, grab a validated IP part, insert a dot
+        // append to the current
+        int _start =start, _end = _start ;
+
+        while(s.begin()+_end != s.end()&& 255>=stoi(string(s.begin()+_start, s.begin()+_end+1))) {
+            string _current(current);
+            _current.insert(_current.end(), s.begin()+_start, s.begin()+_end+1);
+            if (num_parts<3)
+                _current.insert(_current.end(),'.');
+            //std::cout<<current<<std::endl;
+            GetValidIpAddressHelper(s, result,_end+1,num_parts+1,_current);
+            _end++;
+        }
+    }
+}
 
 vector<string> GetValidIpAddress(const string& s) {
-  // Implement this placeholder.
-  return {};
+    vector<string>  result;
+    string current;
+    GetValidIpAddressHelper(s, result,0, 0,current);
+    return result;
 }
 
 bool comp(vector<string> expected, vector<string> result) {
