fe4e97916dc8f40cb437a85ee4f31ee7518c677d
mengxin_zhao <springvesper@gmail.com>
2018-03-17 17:42:34 -0700

    solve nearest repeated entry. judge data passed

--- epi_judge_cpp/nearest_repeated_entries.cc
+++ epi_judge_cpp/nearest_repeated_entries.cc
@@ -1,12 +1,37 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <utility>
+#include <limits>
+#include <iostream>
+#include <unordered_map>
 
 using std::string;
 using std::vector;
+using std::unordered_map;
+using std::priority_queue;
 
+
+// map each word to its the last minimum gap
+// iterate through the map and get the min gap
 int FindNearestRepetition(const vector<string>& paragraph) {
-  // Implement this placeholder.
-  return 0;
+    unordered_map<string, int > tbl;
+    int min_gap = INT_MAX;
+    for(int i=0; i< paragraph.size();i++){
+        if (tbl.find(paragraph[i]) == tbl.end())
+            tbl[paragraph[i]] = i;
+        else {
+            if (min_gap >  (i-tbl[paragraph[i]]))
+                min_gap = i-tbl[paragraph[i]];
+            //std::cout<<paragraph[i]<<"," << i <<" min_gap:" << min_gap<<std::endl;
+            tbl[paragraph[i]] = i;
+        }
+    }
+    if (min_gap == INT_MAX)
+        return -1;
+    else
+        return min_gap;
+    
 }
 
 #include "test_framework/test_utils_generic_main.h"
