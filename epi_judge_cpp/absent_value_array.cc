ea6ff5f8ad63ed04787858c20294d3a6b6a16cc6
mengxin_zhao <springvesper@gmail.com>
2018-03-18 23:47:08 -0700

    solve absent_value_array 12.14. judge data passed

--- epi_judge_cpp/absent_value_array.cc
+++ epi_judge_cpp/absent_value_array.cc
@@ -11,7 +11,7 @@ using std::invalid_argument;
 using std::vector;
 using std::unordered_map;
 
-// uint32_t range  [0,2^32-1] ~ 4 billion
+// uint32_t range  [0,2^32-1]
 // bit for counting each uint32_t 2^32/8 = 512M
 // problem statement has memory restriction <  2 MB ram
 // use method similar to page address
@@ -19,36 +19,36 @@ using std::unordered_map;
 // page_size = 2^16
 // use 2^16 * 4byte to count = 1MB
 // offset_in_the_page = 2^16
-// if one address is missing, one page_index would be less than others
+// there are ~ one billion address, if not missing at all, should be 4 billion address.
+// some address should have all offset in the page. ie. page_indx = 1. offset 0~2^16
+// if one address is missing, its offset number would be less than 2^16
 //
-// the test data is not billion files
+// the test data is not billion files. return the first missing value
 int FindMissingElement(vector<int>::const_iterator stream_begin,
                        const vector<int>::const_iterator& stream_end) {
-    const int page_size = 1024 * 64;
+    const int page_size = 1024 * 64, page_num = 1024 * 64;
     vector<int> count(page_size,0);
-    std::cout<<*stream_begin<<std::endl;
     auto _stream_begin = stream_begin;
     while(_stream_begin!=stream_end){
-        //std::cout<<*_stream_begin<<std::endl;
         count[(*_stream_begin)>>16]++;
         _stream_begin++;
     }
-    int missing_page = *(std::min_element(count.begin(),count.end()));
-    std::cout<<"missing_page: " << missing_page<<std::endl;
-    // now to find offset
-    vector<int> offset_count(page_size,0);
-    _stream_begin = stream_begin;
-    while(_stream_begin!=stream_end){
-        if ((*_stream_begin >> 16 )== missing_page) {
-            offset_count[*_stream_begin & (page_size-1)]++;
+    for (int i=0; i< page_num; i++) {
+        if (count[i] < (1<<16)) {
+            int missing_page = i;
+            vector<int> offset_count(page_size,0);
+            _stream_begin = stream_begin;
+            while(_stream_begin!=stream_end){
+                if ((*_stream_begin >> 16 )== missing_page) {
+                    offset_count[*_stream_begin & (page_size-1)]++;
+                }
+                _stream_begin++;
+            }
+            int missing_offset = std::min_element(offset_count.begin(),offset_count.end()) - offset_count.begin();
+            return missing_page * page_size + missing_offset;
         }
-        _stream_begin++;
     }
-    int missing_offset = *(std::min_element(offset_count.begin(),offset_count.end()));
-//    for( auto & iter: offset_count)
-//        std::cout<<iter<<std::endl;
-    std::cout<<"missing_offset: " << missing_offset<<std::endl;
-    return (missing_page << 16) + missing_offset;
+    return -1 ;// no missing value
 }
 
 int FindMissingElementWrapper(const vector<int>& stream) {
