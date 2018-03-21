5efd73520dba79811f10fe9c0c36bec6496cdfc8
mengxin_zhao <springvesper@gmail.com>
2018-03-21 12:32:28 -0700

    solve minimum_points_covering_intervals. judge data passed

--- epi_judge_cpp/minimum_points_covering_intervals.cc
+++ epi_judge_cpp/minimum_points_covering_intervals.cc
@@ -3,14 +3,42 @@
 #include "test_framework/test_utils_serialization_traits.h"
 
 using std::vector;
+using std::sort;
+using std::min;
+using std::max;
 
 struct Interval {
-  int left, right;
+    int left, right;
+    bool operator < (const Interval &lh) const {
+        return right < lh.right;
+    }
 };
 
+// this is finding the and set of intervals.
+// sort the right point
+// merge = (max(left, new_left), min(right, new_right))
 int FindMinimumVisits(vector<Interval> intervals) {
-  // Implement this placeholder.
-  return 0;
+
+    Interval merged;
+    int min_point = 0;
+    
+    if (intervals.size () == 0)
+        return 0;
+    
+    sort(intervals.begin(),intervals.end());
+    merged = intervals[intervals.size()-1];
+    for (int i = intervals.size()-2; i>=0;i--) {
+        if (intervals[i].right >= merged.left) {
+            merged={ max(merged.left, intervals[i].left), min(merged.right, intervals[i].right) };
+        }else {
+            min_point++;
+            // the point is merged.right
+            merged = intervals[i];
+        }
+    }
+    // the last one
+    min_point++;
+    return min_point;
 }
 
 template <>
