4b0a1b5f220f83408d2fb168bd029e7476c80932
mengxin_zhao <springvesper@gmail.com>
2018-03-21 18:43:44 -0700

    solve sum_root_to_leaf. judge data passed

--- epi_judge_cpp/sum_root_to_leaf.cc
+++ epi_judge_cpp/sum_root_to_leaf.cc
@@ -1,8 +1,26 @@
 #include "binary_tree_node.h"
+#include <iostream>
+
+// recursively add up the all node
+int SumRootToLeafHelper(const unique_ptr<BinaryTreeNode<int>> & node, int curr_sum) {
+    if (node==nullptr) {
+        return curr_sum;
+    }
+    else {
+        curr_sum = curr_sum*2 + node->data;
+        if (node->left && node->right)
+            return SumRootToLeafHelper(node->left,curr_sum) + SumRootToLeafHelper(node->right,curr_sum);
+        else if (node->left && ! node->right)
+            return SumRootToLeafHelper(node->left,curr_sum);
+        else if (!node->left && node->right)
+            return SumRootToLeafHelper(node->right,curr_sum);
+        else return curr_sum;
+    }
+}
 
 int SumRootToLeaf(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // Implement this placeholder.
-  return 0;
+    int sum = SumRootToLeafHelper(tree,0);
+    return sum;
 }
 
 #include "test_framework/test_utils_generic_main.h"
