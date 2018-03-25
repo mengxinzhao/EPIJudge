bfec6bbad3e927c38de629d346b429eee6d85325
mengxin_zhao <springvesper@gmail.com>
2018-03-25 11:20:28 -0700

    solve n_queue problem.judge data passed

--- epi_judge_cpp/n_queens.cc
+++ epi_judge_cpp/n_queens.cc
@@ -4,9 +4,43 @@
 
 using std::vector;
 
+bool loc_exist(int curr_loc, int curr_row,vector<int>existing_loc) {
+    if (find(existing_loc.begin(),existing_loc.end(),curr_loc) != existing_loc.end())
+        return true;
+    else {
+        // check if it leads to a diagnal attacking position
+        for (int i=0;i<existing_loc.size();i++) {
+            if (std::abs(curr_loc - existing_loc[i]) == std::abs(curr_row-i))
+                return true;
+        }
+        
+    }
+    return false;
+}
+
+// kth row to be filled
+void NQueensHelper(vector<vector<int>> &result, vector<int>current, int k, int n) {
+    if (k>=n ) {
+        // have a solution
+        if (current.size() == n)
+            result.push_back(current);
+        return;
+    }
+    int curr_choice = current.back();
+    for (int i=0;i<n;i++) {
+        vector<int> copy = current;
+        if (!loc_exist(i,k, copy)) {
+            copy.push_back(i);
+            NQueensHelper(result,copy,k+1,n);
+        }
+    }
+}
 vector<vector<int>> NQueens(int n) {
-  // Implement this placeholder.
-  return {};
+    vector<vector<int>> result;
+    for (int i=0;i<n;i++) {
+        NQueensHelper(result,vector<int>({i}),1,n);
+    }
+    return result;
 }
 
 bool Comp(vector<vector<int>>& a, vector<vector<int>>& b) {
