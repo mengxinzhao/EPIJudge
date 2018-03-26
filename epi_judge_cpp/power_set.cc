4537d373e1a1261bdf28a3dd4581b9dd8084438b
mengxin_zhao <springvesper@gmail.com>
2018-03-25 22:33:57 -0700

    solve generating power set using DP

--- epi_judge_cpp/power_set.cc
+++ epi_judge_cpp/power_set.cc
@@ -1,7 +1,10 @@
 #include <vector>
 #include <set>
+#include <utility>
+#include <iostream>
 
 using std::vector;
+using std::move;
 
 void generateSet(vector<vector<int>> &results,const vector<int> input_set,vector<int> curr_set, int start, bool chosen ) {
 	// set 6 4 5
@@ -34,26 +37,48 @@ void generateSet(vector<vector<int>> &results,const vector<int> input_set,vector
 }
 
 vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
-  // Implement this placeholder.
 	// set 4 5 6
-	//     O, {4},{5},{6}, {4,5}, {5,6}, {4,6}, {4,5,6}
+    //     {}, {4},{5},{6}, {4,5}, {5,6}, {4,6}, {4,5,6}
 	//
-	vector<vector<int>> results;
-	if (input_set.empty())
-		return {{}};
+    vector<vector<int>> results = generate_set(input_set,input_set.size());
+    if (input_set.empty())
+        return {{}};
 
-	vector<int> first ({});
+    vector<int> first ({});
 
-	generateSet(results,input_set,first,0,true);
-	generateSet(results,input_set,first,0,false);
+    generateSet(results,input_set,first,0,true);
+    generateSet(results,input_set,first,0,false);
   return results;
+}
+
+// subset[i] = A[i] U subset[i-1]
+// subset[i] = {}
+// i: number of element
+// subset[i-1] element 2^(i-1)
+// O(n * 2^(n)) run time complexity
+vector<vector<int>> GeneratePowerSet2(const vector<int>& input_set) {
+
+    vector<vector<int>> subset;
+    vector<vector<int>> result({{}});
+    
+    for (int i=1;i<=input_set.size();i++) {
+        subset = move(result);
+        for (int j=0; j< subset.size();j++) {
+            // subset[i] = A[i] U subset[i-1]
+            result.push_back(subset[j]);
+            subset[j].push_back(input_set[i-1]);
+            result.push_back(subset[j]);
+            
+        }
+    }
+    return result;
 }
 
 #include "test_framework/test_utils_generic_main.h"
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> param_names{"input_set"};
-  generic_test_main(argc, argv, param_names, "power_set.tsv", &GeneratePowerSet,
+  generic_test_main(argc, argv, param_names, "power_set.tsv", &GeneratePowerSet2,
                     &UnorderedComparator<vector<vector<int>>>);
   return 0;
 }
