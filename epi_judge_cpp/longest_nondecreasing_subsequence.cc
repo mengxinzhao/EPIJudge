3cbf1de54af9b9204ce6898e56c08b9b7d75fb99
mengxin_zhao <springvesper@gmail.com>
2018-04-01 22:57:26 -0700

    initial implementation of longest_nondecreasing_subsequence. only passed partitial judge data

--- epi_judge_cpp/longest_nondecreasing_subsequence.cc
+++ epi_judge_cpp/longest_nondecreasing_subsequence.cc
@@ -1,10 +1,47 @@
 #include <vector>
-
+#include <cstddef>
+#include <algorithm>
+#include <iostream>
+#include <utility>
 using std::vector;
-
+using std::max;
+using std::min;
+using std::pair;
+// LSN[i] = max(sequence from (i+1...N), sequence from (k ...N))  k is the first element A[k] > A[i]
+//  when A[i] > A[i-1] in LSN[i+1]
+// else LSN[i] =  LSN[i+1]+1 when A[i] <= A[i-1]
 int LongestNondecreasingSubsequenceLength(const vector<int>& A) {
-  // Implement this placeholder.
-  return 0;
+    vector<pair<int,int>> LSN(A.size(),pair<int,int>(0,0));
+    LSN[A.size()-1] = {A[A.size()-1],1};
+    int curr_min = A[A.size()-1];
+    for (int i = A.size()-2;i>=0;i--) {
+        //std::cout<<"curr_min: " << curr_min<<" ";
+        if (A[i] >curr_min) {
+            //
+            int num=0;
+            int j=i+1;
+            int max_after_i = 0;
+            for (; j<A.size();j++){
+                if (A[j]>=A[i] && A[j]<=LSN[j].first) {
+                   max_after_i = max(max_after_i,LSN[j].second +1);
+                }
+            }
+            if (LSN[i+1].second > max_after_i) {
+                LSN[i]= LSN[i+1];
+            }
+            else {
+                LSN[i]= {A[i], max_after_i};
+                curr_min = A[i];
+            }
+         }
+        else {
+            LSN[i].second = LSN[i+1].second + 1;
+            LSN[i].first = A[i];
+            curr_min = A[i];
+        }
+        //std::cout<<"i:" << i << " LSN: " <<LSN[i].first<<", "<<  LSN[i].second << std::endl;
+    }
+    return LSN[0].second;
 }
 
 #include "test_framework/test_utils_generic_main.h"
