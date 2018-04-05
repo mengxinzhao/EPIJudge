c457a5d008cac13daff5ace7b77b7f6619934311
mengxin_zhao <springvesper@gmail.com>
2018-04-05 12:39:47 -0700

    solve the max_square_submatrix. judge data passed

--- epi_judge_cpp/max_square_submatrix.cc
+++ epi_judge_cpp/max_square_submatrix.cc
@@ -17,6 +17,7 @@ struct rectangle {
 };
 
 // use two additinoal helper tables
+// O(N*M*k) run time complexity k is the max width of square
 int MaxSquareSubmatrix(const vector<vector<int>>& A) {
     // tbl[i][j]: how far can a square reach to its right and down from i,j
     vector<vector<int>> tbl(A);
@@ -32,30 +33,22 @@ int MaxSquareSubmatrix(const vector<vector<int>>& A) {
         for (int j =1; j < A[i].size();j++)
             r_tbl[i][j] = A[i][j]? min(r_tbl[i-1][j], r_tbl[i][j-1])+1:  0;
     }
-#if 0
-    std::cout<<std::endl;
-    for (int i=0; i< tbl.size();i++) {
-        for (int j =0; j<tbl[i].size();j++)
-            std::cout<<tbl[i][j] << " " ;
-        std::cout<<std::endl;
-    }
-    std::cout<<"======================"<<std::endl;
-    for (int i=0; i< tbl.size();i++) {
-        for (int j =0; j<tbl[i].size();j++)
-            std::cout<<r_tbl[i][j] << " " ;
-        std::cout<<std::endl;
-    }
-#endif
     
     // loop the table first
     int max_size = 0;
     for (int i=0; i< A.size();i++) {
         for (int j=0; j < A[i].size();j++) {
-            if (tbl[i][j] > max_size) {
+            if (tbl[i][j]) {
                 // check further if its bottom right can reach back
-                int h = min(i+ tbl[i][j], (int)A.size())-1;
-                int w = min(j + tbl[i][j], (int)A[i].size())-1;
-                max_size = (min(tbl[i][j],r_tbl[h][w]) == tbl[i][j])?tbl[i][j] : max_size;
+                for (int k = tbl[i][j] ; k>=1;k--){
+                    int h = min(i+ k, (int)A.size())-1;
+                    int w = min(j + k, (int)A[i].size())-1;
+                    //std::cout<<"i,j: "<< i<<", "<<j<<" tbl: "<< tbl[i][j]<<" r_tbl: "<< r_tbl[h][w] <<std::endl;
+                    if (r_tbl[h][w]>=k && k >max_size) {
+                        max_size = k;
+                    }
+                }
+
             }
         }
     }
