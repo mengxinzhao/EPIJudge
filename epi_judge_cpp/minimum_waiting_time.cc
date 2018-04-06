525d2269197fb0b54022828176f04b4517671a52
mengxin_zhao <springvesper@gmail.com>
2018-04-05 22:45:53 -0700

    solve minimum_waiting_time. judge data passed

--- epi_judge_cpp/minimum_waiting_time.cc
+++ epi_judge_cpp/minimum_waiting_time.cc
@@ -1,10 +1,19 @@
 #include <vector>
-
+#include <algorithm>
 using std::vector;
+using std::sort;
 
+// waiting time till ith request
+// W[i]  =  sum(t[j])  j=0...i-1
+// TotalTime = sum(W[i])
+// leave the biggest service time item out of the sum equation
 int MinimumTotalWaitingTime(vector<int> service_times) {
-  // Implement this placeholder.
-  return 0;
+    
+    sort(service_times.begin(), service_times.end());
+    int wait_time = 0;
+    for (int i=0;i<service_times.size();i++)
+        wait_time += service_times[i] *(service_times.size()-(i+1));
+    return wait_time;
 }
 
 #include "test_framework/test_utils_generic_main.h"
