f98752c64fd17b75da6c942a4ebb55506ae14be1
mengxin_zhao <springvesper@gmail.com>
2018-04-08 15:19:46 -0700

    solve majority_element. judge data passed

--- epi_judge_cpp/majority_element.cc
+++ epi_judge_cpp/majority_element.cc
@@ -1,13 +1,22 @@
 #include <string>
 #include <vector>
-
+#include <map>
+#include <iostream>
 using std::string;
 using std::vector;
-
+using std::map;
 string MajoritySearch(vector<string>::const_iterator stream_begin,
                       const vector<string>::const_iterator stream_end) {
-  // Implement this placeholder.
-  return "";
+    map<string, int> words_count;
+    map<int, string> count_words;
+    auto &iter = stream_begin;
+    while(iter!= stream_end) {
+        words_count[*iter]++;
+        count_words[words_count[*iter]++] = *iter;
+        iter++;
+    }
+    
+    return count_words.rbegin()->second;
 }
 
 string MajoritySearchWrapper(const vector<string>& stream) {
