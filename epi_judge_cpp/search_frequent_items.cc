f5e0b475cacb2e9b72bf8ba271cd424a0ddb729e
mengxin_zhao <springvesper@gmail.com>
2018-04-09 00:21:16 -0700

    use Misra-Gries algorithm to solve heavy hitter ( though not fully understand the math).judge data passed

--- epi_judge_cpp/search_frequent_items.cc
+++ epi_judge_cpp/search_frequent_items.cc
@@ -8,7 +8,7 @@ using std::string;
 using std::vector;
 using std::map;
 
-// Finds the candidates which may occur > n / k times.
+// Finds the candidates which may occur >= n / k times.
 vector<string> SearchFrequentItems(
     int k, vector<string>::const_iterator stream_begin,
     const vector<string>::const_iterator stream_end) {
@@ -22,16 +22,73 @@ vector<string> SearchFrequentItems(
         count++;
         iter++;
     }
-    iter = stream_begin;
     for (const auto & iter: word_count){
-        if (iter.second > count/k )
+        if (iter.second >=count/k )
             hitters.push_back(iter.first);
     }
     return hitters;
 }
 
+// candidate occurs > n/k
+// at most k outputs
+// Misra-Gries heavy hitter algorithm
+// O(k) memory
+// algorithm from lecture handout
+// http://people.csail.mit.edu/moitra/docs/6854lec4.pdf
+vector<string> SearchFrequentItems_MG(
+   int k, vector<string>::const_iterator stream_begin,
+    const vector<string>::const_iterator stream_end) {
+    
+    map<string, int> distinct_set;
+    vector<string> hitters;
+    int count =0;
+    auto iter = stream_begin;
+
+    while(iter!= stream_end) {
+        if (distinct_set.find(*iter)!= distinct_set.end())
+            distinct_set[*iter]++;
+        else if (distinct_set.size() <=k)
+                distinct_set[*iter]++;
+        else {
+            // throw away item
+            auto iterator = distinct_set.begin();
+            while(iterator != distinct_set.end()) {
+                iterator->second--;
+                if (iterator->second == 0){
+                    auto to_del = iterator;
+                    iterator++;
+                    distinct_set.erase(to_del);
+                }
+                else
+                    iterator++;
+            }
+        }
+        iter++;
+        count++;
+    }
+    
+    for ( auto & elem: distinct_set) {
+        elem.second = 0;
+        //std::cout<< elem.first<< ", ";
+    }
+    //std::cout<<std::endl;
+    
+    iter = stream_begin;
+    while(iter!= stream_end) {
+        if (distinct_set.find(*iter)!= distinct_set.end())
+            distinct_set[*iter]++;
+        iter++;
+    }
+    //std::cout<<"count/k :" <<float(count)/k << std::endl;
+    for (const auto & elem: distinct_set){
+        if (elem.second > float(count)/k)
+            hitters.push_back(elem.first);
+    }
+    return hitters;
+}
+
 vector<string> SearchFrequentItemsWrapper(int k, vector<string>& stream) {
-  return SearchFrequentItems(k, begin(stream), end(stream));
+  return SearchFrequentItems_MG(k, begin(stream), end(stream));
 }
 
 #include "test_framework/test_utils_generic_main.h"
