4e249b7ede5eac43c3adc515895a6270b49f6940
mengxin_zhao <springvesper@gmail.com>
2018-04-12 23:01:29 -0700

    solve search_maze by DFS

--- epi_judge_cpp/search_maze.cc
+++ epi_judge_cpp/search_maze.cc
@@ -1,27 +1,62 @@
 #include <istream>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <utility>
+#include <algorithm>
 
 #include "test_framework/test_failure_exception.h"
 #include "test_framework/test_timer.h"
 #include "test_framework/test_utils_serialization_traits.h"
 
 using std::vector;
+using std::pair;
 
 typedef enum { WHITE, BLACK } Color;
 
 struct Coordinate {
-  bool operator==(const Coordinate& that) const {
-    return x == that.x && y == that.y;
-  }
+    bool operator==(const Coordinate& that) const {
+        return x == that.x && y == that.y;
+    }
 
-  int x, y;
+    int x, y;
+    Coordinate(int _x, int _y):x(_x),y(_y) {}
+    
 };
+
+bool NextStep(vector<vector<Color>>maze, vector<vector<int>>&visited,vector<Coordinate> &path,Coordinate current, const Coordinate &end) {
+    
+    //std::cout<<"trying..."<<current.x << ", "<< current.y << std::endl;
+    if (current == end) {
+        return true;
+    }
+    //look at viable steps and make a choice
+    vector<pair<int,int>> options = {{current.x, current.y+1}, {current.x, current.y-1},{current.x+1,current.y},{current.x-1,current.y} };
+    for (auto &iter: options){
+        int x = iter.first;
+        int y = iter.second;
+        // the test data requires maze[x][y] instead of usual matrix[row][col] convension
+        //if (x>=0 && y>=0 && y<maze.size() &&   x < maze[y].size() && maze[y][x] == WHITE && !visited[y][x]){
+        if (x>=0 && y>=0 && x<maze.size() &&   y < maze[x].size() && maze[x][y] == WHITE && !visited[x][y]){
+            visited[x][y]= 1;
+            if (NextStep(maze,visited,path,{x,y},end)) {
+                path.emplace_back(x,y);
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 vector<Coordinate> SearchMaze(vector<vector<Color>> maze, const Coordinate& s,
                               const Coordinate& e) {
-  // Implement this placeholder.
-  return {};
+    vector<Coordinate>path;
+    vector<vector<int>>visited(maze.size(),vector<int>(maze[0].size(),0));
+    if (NextStep(maze,visited,path,s,e))
+        path.push_back(s);
+    std::reverse(path.begin(), path.end());
+
+    return path;
 }
 
 template <>
