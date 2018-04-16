30ba4fc3a747f862ca5145d920828134d09f5fe3
mengxin_zhao <springvesper@gmail.com>
2018-04-15 18:28:43 -0700

    initial solution for defective jugs. not passing judge data

--- epi_judge_cpp/defective_jugs.cc
+++ epi_judge_cpp/defective_jugs.cc
@@ -1,16 +1,39 @@
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
 #include "test_framework/test_utils_serialization_traits.h"
 
 using std::vector;
-
+using std::sort;
 struct Jug {
-  int low, high;
+    int high;
+    int low;
 };
+// if [510,515] jug is used in the final step, the already measured milk
+// needs to be ([2100-510], [2300-515]. The last stage remain milk if not fitting the size of any
+// jugs will not be feasible to measure milk.
+// [L,H] feasible  = [L-550, H-515]  || || [L-290, H-310| ||  [ L- 230, H- 240]
+// L,H feasible =  OR ( (L - jugs[i].low)feasible && (L - jugs[i].high) feasible) i=0...N
+// use DP[L][H] = true/false indicate if a L,H range is feasible to be measured by the jugs
+// DP[0][0] = true
+// DP[jugs[i].high][DP[jugs[i]].low] = true for all i=0...N
 
-bool CheckFeasible(const vector<Jug>& jugs, int L, int H) {
-  // Implement this placeholder.
-  return true;
+bool CheckFeasible(const vector<Jug>& Jugs, int L, int H) {
+    vector<vector<bool>> feasible(H+1, vector<bool>(L+1,false));
+    // init
+
+    feasible[0][0] = true;
+
+    for (int h = 1; h<= H; h++) {
+        for (int l = 1; l <= L; l++) {
+            for (size_t k=0; k< Jugs.size();k++) {
+                feasible[h][l] = feasible[h][l] ||( (h-Jugs[k].high>=0 && l-Jugs[k].low>=0)?  feasible[h-Jugs[k].high][l-Jugs[k].low] : false);
+            }
+//            if (feasible[h][l] == true)
+//                std::cout<<"["<<h<<","<<l<< "]: "<<feasible[h][l]<<std::endl;
+        }
+    }
+    return feasible[H][L];
 }
 
 template <>
