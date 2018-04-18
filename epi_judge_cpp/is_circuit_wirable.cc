44c474b9af570e09c3e98dcaf671a923af364694
mengxin_zhao <springvesper@gmail.com>
2018-04-17 23:18:20 -0700

    solve is_circuit_wirable using BFS search. judge data passed

--- epi_judge_cpp/is_circuit_wirable.cc
+++ epi_judge_cpp/is_circuit_wirable.cc
@@ -1,19 +1,84 @@
 #include <stdexcept>
 #include <vector>
-
+#include <unordered_set>
+#include <iostream>
+#include <functional>
+#include <queue>
 #include "test_framework/test_timer.h"
 #include "test_framework/test_utils_serialization_traits.h"
 
 using std::vector;
+using std::queue;
+using std::hash;
+using std::unordered_set;
 
 struct GraphVertex {
   int d = -1;
   vector<GraphVertex*> edges;
 };
 
+struct PinPlacement {
+    GraphVertex *vertex;
+    bool left;
+    PinPlacement (GraphVertex *_v, bool _left):vertex(_v),left(_left) {}
+    bool operator == (const PinPlacement &p) const {
+        return (vertex == p.vertex && left ==p.left);
+    }
+};
+
+struct PinPlacementComapre {
+    bool operator ()(const PinPlacement &p1, const PinPlacement &p2){
+        return (p1.vertex == p2.vertex && p1.left ==p2.left);
+    }
+};
+
+struct PinPlacementHasher{
+    size_t operator()(const PinPlacement &pin) const {
+        auto addr = reinterpret_cast<uintptr_t>(pin.vertex);
+        // hash poiter.
+        return addr ^ (1021 * pin.left );
+    }
+};
+
+// this is a hard BFS traversal question
+// a pair pin can be made to the spec when they are from BFS invoking u --> discovery v
+// if there is a wire connection  between vertices both in u's adjacement list it is not feasible
+// to place the pin.
 bool IsAnyPlacementFeasible(vector<GraphVertex>* graph) {
-  // Implement this placeholder.
-  return true;
+    unordered_set<PinPlacement,PinPlacementHasher, PinPlacementComapre> placed;
+    queue<PinPlacement>graph_q;
+    graph_q.emplace(&(*graph)[0],true);
+    placed.emplace(&(*graph)[0], true);
+    int i=1;
+    while (i< graph->size()) {
+        while(!graph_q.empty()) {
+            PinPlacement u = graph_q.front();
+            graph_q.pop();
+            for (GraphVertex * v: u.vertex->edges) {
+                // both vertices are seen but there is an edge between. check the placement.
+                // in order to have cross edge between left and right, it needs v->left= !u->left
+                // if we find {v,u->left} in the placed set that shows the pin already placed
+                // and can't satisfy the current request.
+                if (placed.find({v,u.left }) != placed.end())
+                    return false;
+                else if (placed.find({v,!u.left}) == placed.end()) {
+                    placed.emplace(v, !u.left);
+                    graph_q.emplace(v, !u.left);
+                }
+            }
+        }
+        if (placed.find({&(*graph)[i], true})!= placed.end()
+            || placed.find({&(*graph)[i], false })!= placed.end())
+            i++;
+        else {
+            // this node is isolated doesn't matter where it is put.
+            graph_q.emplace( &(*graph)[i], true);
+            placed.emplace(&(*graph)[i], true);
+            
+        }
+        
+    }
+    return true;
 }
 
 struct Edge {
