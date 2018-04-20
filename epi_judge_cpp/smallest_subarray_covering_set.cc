61497f0cac6e5d278bf140b9882bc9aaca60a677
mengxin_zhao <springvesper@gmail.com>
2018-04-20 14:34:30 -0700

    solve smallest_subarray_covering_set. judege data passed

--- epi_judge_cpp/smallest_subarray_covering_set.cc
+++ epi_judge_cpp/smallest_subarray_covering_set.cc
@@ -1,6 +1,9 @@
 #include <string>
 #include <unordered_set>
 #include <vector>
+#include <unordered_map>
+#include <climits>
+#include <iostream>
 
 #include "test_framework/test_failure_exception.h"
 #include "test_framework/test_timer.h"
@@ -8,15 +11,43 @@
 using std::string;
 using std::unordered_set;
 using std::vector;
-
+using std::unordered_map;
+using std::min;
 struct Subarray {
   int start, end;
 };
 
+
+struct Location {
+    int start, end;
+    int length;
+};
+
+// O(N*M) time: N: number of words in paragraph, M: number of words in keywords
 Subarray FindSmallestSubarrayCoveringSet(
     const vector<string> &paragraph, const unordered_set<string> &keywords) {
-  // Implement this placeholder.
-  return {0, 0};
+    // track the  appearing loc for each keyword
+    unordered_map<string, int> cache;
+    Location  min_length = {.length = INT_MAX};
+    for (int i=0;i< paragraph.size();i++) {
+        if (keywords.find(paragraph[i]) != keywords.end()) {
+            cache[paragraph[i]] = i;
+        }
+        int min_loc = INT_MAX, max_loc = INT_MIN;
+        if (cache.size() == keywords.size()) {
+            for (auto  &iter: cache) {
+                if (min_loc > iter.second)
+                    min_loc = iter.second;
+                if (max_loc < iter.second)
+                    max_loc = iter.second;
+            }
+            if (min_length.length > max_loc - min_loc) {
+                min_length = {min_loc, max_loc, max_loc-min_loc};
+            }
+        }
+    }
+
+  return {min_length.start,min_length.end};
 }
 
 int FindSmallestSubarrayCoveringSetWrapper(
