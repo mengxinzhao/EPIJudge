402472d0101c02955ecf47aaef9a381d35e63c33
mengxin_zhao <springvesper@gmail.com>
2018-04-20 15:15:50 -0700

    solve smallest_subarray_covering_all_values. not efficient. judge data passed

--- epi_judge_cpp/smallest_subarray_covering_all_values.cc
+++ epi_judge_cpp/smallest_subarray_covering_all_values.cc
@@ -1,21 +1,53 @@
 #include <string>
 #include <vector>
-
+#include <unordered_map>
+#include <climits>
+#include <algorithm>
 #include "test_framework/test_failure_exception.h"
 #include "test_framework/test_timer.h"
 
 using std::string;
 using std::vector;
+using std::unordered_map;
+using std::min;
 
 struct Subarray {
   // Represent subarray by starting and ending indices, inclusive.
   int start, end;
 };
 
+struct Tracker {
+    int start;
+    int expect;
+};
+
+//O(N^2) running time
+// not very efficient
 Subarray FindSmallestSequentiallyCoveringSubset(
     const vector<string>& paragraph, const vector<string>& keywords) {
-  // Implement this placeholder.
-  return {0, 0};
+    int min_length = INT_MAX;
+    int start;
+    int end;
+    for(int i=0;i < paragraph.size();i++) {
+        if (paragraph[i] == keywords[0]) {
+            Tracker tk = {.start = i, .expect = 1};// expecting next
+            int j= i+1;
+            while (j< paragraph.size()) {
+                if (paragraph[j] == keywords[tk.expect]){
+                    tk.expect++;
+                }
+                if (tk.expect == keywords.size() &&  j - tk.start < min_length) {
+                    min_length = j-tk.start;
+                    start = tk.start;
+                    end = j;
+                    break;
+                }
+                j++;
+            }
+        }
+    }
+    
+  return {start, end};
 }
 
 int FindSmallestSequentiallyCoveringSubsetWrapper(
