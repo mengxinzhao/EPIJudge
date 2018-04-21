95a5b1309360f79909678a5b930185c286bb883a
mengxin_zhao <springvesper@gmail.com>
2018-04-21 14:46:33 -0700

    improve smallest_subarray_covering_all_values by using DP. linear time in O notation in theory. Real time performance not beating the 1st method. judge data passed

--- epi_judge_cpp/smallest_subarray_covering_all_values.cc
+++ epi_judge_cpp/smallest_subarray_covering_all_values.cc
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include <climits>
 #include <algorithm>
+#include <utility>
+#include <iostream>
 #include "test_framework/test_failure_exception.h"
 #include "test_framework/test_timer.h"
 
@@ -10,6 +12,8 @@ using std::string;
 using std::vector;
 using std::unordered_map;
 using std::min;
+using std::pair;
+using std::next;
 
 struct Subarray {
   // Represent subarray by starting and ending indices, inclusive.
@@ -48,6 +52,52 @@ Subarray FindSmallestSequentiallyCoveringSubset(
     }
     
   return {start, end};
+}
+
+
+// O(n) time running time
+// almost bellman-ford shortest path algorithm
+// the difference is that each node needs to be sequential hop from i-1 to i and each node i
+// has one edge to the next i+1. thus  no outer loop in bellman-ford's algorithm
+// shortest_dist [i] = shortest_dist[i-1] +loc_cache[i] - loc_cache[i-1]
+Subarray FindSmallestSequentiallyCoveringSubset_Linear(
+     const vector<string>& paragraph, const vector<string>& keywords){
+    int start;
+    int end;
+    int min_length = INT_MAX;
+    unordered_map<string, int> kw_cache;// map each key word to its location
+    // keyword's latest occurance in paragraph
+    vector<int> loc_tracker(keywords.size(), -1) ;
+    // shortest length for distance from keyword 0 to  keyword i
+    vector<int> shortest_dist(keywords.size(), INT_MAX);
+    
+    for (int i=0; i< keywords.size();i++)
+        kw_cache[keywords[i]] = i;
+    
+    for (int i=0; i< paragraph.size();i++) {
+        if (kw_cache.find(paragraph[i])!= kw_cache.end()) {
+            //std::cout<<"checking ..."<<paragraph[i] << " @" << i <<std::endl;
+            int expected_loc = kw_cache[paragraph[i]];
+            // update location
+            loc_tracker[expected_loc] =i;
+            if (expected_loc == 0)
+                shortest_dist[0] =1;
+            else if (shortest_dist[expected_loc-1]!= INT_MAX){
+                shortest_dist [expected_loc] = shortest_dist[expected_loc-1] + loc_tracker[expected_loc]  - loc_tracker[expected_loc-1];
+                //std::cout<<"shortest to  ..."<<keywords[expected_loc]<< " = " << shortest_dist [expected_loc]  <<std::endl;
+            }
+
+            if (expected_loc ==keywords.size()-1) {
+                if (shortest_dist [expected_loc] < min_length) {
+                    min_length = shortest_dist[expected_loc];
+                    start = loc_tracker[expected_loc] - shortest_dist [expected_loc]+1;
+                    end = loc_tracker[expected_loc];
+                    //std::cout<<"shortest to  ..."<<keywords[expected_loc]<< " start: " <<start<<" end: "<<end <<std::endl;
+                }
+            }
+        }
+    }
+    return {start, end};
 }
 
 int FindSmallestSequentiallyCoveringSubsetWrapper(
