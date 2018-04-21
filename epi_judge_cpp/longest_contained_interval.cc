aac0701681ea9059a04ca2cb8d48d66669ca78fe
mengxin_zhao <springvesper@gmail.com>
2018-04-20 17:15:10 -0700

    solve longest_contained_interval. not optimal but judge data passed.

--- epi_judge_cpp/longest_contained_interval.cc
+++ epi_judge_cpp/longest_contained_interval.cc
@@ -1,10 +1,34 @@
 #include <vector>
-
+#include <algorithm>
+#include <unordered_map>
+#include <climits>
 using std::vector;
+using std::max;
+using std::unordered_map;
+using std::sort;
 
-int LongestContainedRange(const vector<int>& A) {
-  // Implement this placeholder.
-  return 0;
+// sort the vector
+// the problem reduced to find the longest consective subarray -remove duplicated
+// O(nlogn) running time
+int LongestContainedRange(vector<int>& A) {
+
+    sort(A.begin(), A.end());
+    int start = 0;
+    int max_length = INT_MIN;
+    int i=0;
+    int dup=0;
+    while(i<A.size()-1) {
+        if (A[i+1] - A[i] > 1) {
+            max_length = max(max_length, i-start+1 - dup);
+            start = i+1;
+            dup = 0;
+        }else if (A[i+1]== A[i])
+            dup++;
+        i++;
+    }
+    // the last one
+    max_length = max(max_length, i-start+1-dup);
+    return max_length;
 }
 
 #include "test_framework/test_utils_generic_main.h"
