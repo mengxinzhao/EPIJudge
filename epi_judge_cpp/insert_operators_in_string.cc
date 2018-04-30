302abfa2a3aec016c80164d009ddff5e1124076b
mengxin_zhao <springvesper@gmail.com>
2018-04-29 22:38:27 -0700

    first implementation of express synthesis.not passing all judge data

--- epi_judge_cpp/insert_operators_in_string.cc
+++ epi_judge_cpp/insert_operators_in_string.cc
@@ -1,10 +1,39 @@
 #include <vector>
+#include <unordered_map>
 
 using std::vector;
+using std::unordered_map;
+
+// DP[i,j] denotes target i can be reached by using up to [0,jth] digit.With j+1th digit, possible numbers are
+// DP[n,i] * 10 + digits[j+1] or DP[n,i] + digits[j+1] or  DP[n,i] * digits[j+1]
+// base case
+// DP[i,0] = true if digits[0] == i else false
+
+//
+bool ExpressionSynthesisHelper(const vector<int> & digits, int i, int target,unordered_map<int, int> &cache) {
+    if (i==0 ) {
+        if (target == digits[0])
+            return true;
+        else
+            return false;
+    }
+    if (cache.find(target) != cache.end())
+        return false;
+    
+    // try all possible candidates
+    vector <int> new_targets = {target - digits[i], (target - digits[i])/10, target / digits[i]};
+    for (auto candidate: new_targets ) {
+        if (ExpressionSynthesisHelper(digits, i-1, candidate,cache) == true)
+            return true;
+    }
+    // cache unsuccessful candidate
+    cache[target] = i;
+    return false;
+}
 
 bool ExpressionSynthesis(const vector<int>& digits, int target) {
-  // Implement this placeholder.
-  return true;
+    unordered_map<int, int> cache;// to cache  unsuccessful try when a target k can't archieved by using up to ith digits
+    return ExpressionSynthesisHelper(digits, digits.size()-1, target,cache);
 }
 
 #include "test_framework/test_utils_generic_main.h"
