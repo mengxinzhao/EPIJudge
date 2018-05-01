fe9cb2db1cffbe11a0744a7040885dc7a4a96472
mengxin_zhao <springvesper@gmail.com>
2018-04-30 23:32:53 -0700

    revisit search_first_key. O(log(n))solution. judge data passed

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -2,18 +2,7 @@
 
 using std::vector;
 
-// this is O(n) solution
-int SearchFirstOfK(const vector<int>& A, int k) {
-	for (int i=0;i<A.size();i++) {
-		if (A[i] == k)
-			return i;
-	}
-  return -1;
-}
-// 0 1 1 4 4 4 5 9 10 10 12 12 14
-// search 4
-// return 2
-// size 13
+// log(n) solution
 int  SearchFirstOfK_BST(const vector<int> &A, int k) {
 	int left = 0, right = A.size()-1, mid = 0;
 	while(left<=right) {
@@ -21,20 +10,18 @@ int  SearchFirstOfK_BST(const vector<int> &A, int k) {
 		mid = left + (right-left)/2;
 		if (A[mid] > k)
             right = mid-1;
-		else if (A[mid] == k)
-            break;
+        else if (A[mid] == k) {
+            if (mid == 0 || A[mid-1]!= A[mid])
+                break;
+            else
+                mid ++;
+        }
 		else
             left = mid + 1 ;
 	}
 	if (left > right)
 		return -1;
 
-	//search from [left , mid -1]
-	int i = mid-1;
-	while(i >=left && A[i] == k )
-		i--;
-	if (i!=mid-1) // first occurence  on the left side
-		return i+1;
 	return mid;
 
 }
