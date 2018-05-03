d80c88b5f539e9a63eaf446ec5d890df16f83008
mengxin_zhao <springvesper@gmail.com>
2018-05-02 23:32:50 -0700

    solve descendant_and_ancestor_in_bst problem. judge data passed

--- epi_judge_cpp/descendant_and_ancestor_in_bst.cc
+++ epi_judge_cpp/descendant_and_ancestor_in_bst.cc
@@ -7,11 +7,51 @@
 using std::unique_ptr;
 
 bool PairIncludesAncestorAndDescendantOfM(
-    const unique_ptr<BstNode<int>>& possible_anc_or_desc_0,
-    const unique_ptr<BstNode<int>>& possible_anc_or_desc_1,
+    const unique_ptr<BstNode<int>>& possible_anc_or_desc_0, //A
+    const unique_ptr<BstNode<int>>& possible_anc_or_desc_1, // B
     const unique_ptr<BstNode<int>>& middle) {
-  // Implement this placeholder.
-  return true;
+    
+    auto *p = possible_anc_or_desc_0.get();
+    auto *q = possible_anc_or_desc_1.get();
+    // after this loop, if middle is not between A,B, one of p,q will be at lower node of A,B
+    // if middle is between A,B one of q,p will be pointing to middle
+    while (p!= possible_anc_or_desc_1.get() && p!= middle.get()&& q!= possible_anc_or_desc_0.get() && q!= middle.get() && (p || q)) {
+        if (p) {
+            p = p->data > middle->data?  p->left.get(): p->right.get();
+        }
+        if (q) {
+            q = q->data > middle->data ? q->left.get(): q->right.get();
+        }
+    }
+    //
+    if ((p != middle.get() && q != middle.get()) ||
+        p == possible_anc_or_desc_1.get() ||
+        q == possible_anc_or_desc_0.get()) {
+        return false;
+    }
+    
+    if (p == middle.get()){ // p started from A
+        //use p to find B
+        while (p) {
+            if (p->data > possible_anc_or_desc_1->data)
+                p = p->left.get();
+            else if (p->data == possible_anc_or_desc_1->data)
+                return true;
+            else
+                p = p->right.get();
+        }
+    }else if (q == middle.get()) { // q started from B
+        // use q to find A
+        while (q) {
+            if (q->data > possible_anc_or_desc_0->data)
+                q = q->left.get();
+            else if (q->data == possible_anc_or_desc_0->data)
+                return true;
+            else
+                q = q->right.get();
+        }
+    }
+    return false;
 }
 
 bool PairIncludesAncestorAndDescendantOfMWrapper(
